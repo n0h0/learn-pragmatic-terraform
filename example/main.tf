dfdca1f9afefca2b289d67c481354753109f6162
n0h0 <kusakatsubasa@gmail.com>
2021-10-19 15:10:52 +0900

    Learned Chapter 3

--- example/main.tf
+++ example/main.tf
@@ -1,10 +1,8 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
+module "web_server" {
+  source        = "./httpd_server"
   instance_type = "t3.micro"
+}
 
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+output "public_dns" {
+  value = module.web_server.public_dns
 }
